9fbb66c9f062ff993361f5da01dc157c3069113b
ADiggins <ajpdiggins@gmail.com>
2025-12-08 18:53:07 +1100

    Week 9 Progress
    Added a slideshow animation effect using WAAPI

--- GamePage.js
+++ GamePage.js
@@ -10,6 +10,47 @@ function displaySlide( newIndex ) {
     slides[activeSlide].style.display = "block";
 }
 
-function changeSlide( by ) {
-    displaySlide (activeSlide + by);
+function displaySlideWithAnimation( newIndex, direction ) {
+    animatedSlideOut( slides[activeSlide], direction );
+    activeSlide = (newIndex+slides.length) % slides.length;
+    animatedSlideIn( slides[activeSlide], direction );
 }
+
+function changeSlide( by ) {
+    displaySlideWithAnimation (activeSlide + by, by > 0 ? 'right':'left');
+}
+
+function animatedSlideIn(slide, direction) {
+    slide.style.display = "block";
+    slide.animate( 
+    [
+        { transform: `translateX(${ direction == 'right' ? '500px':'-500px' } )` },
+        { transform: 'translateX(0px)'}
+    ], 
+    {
+        duration: 600,
+        // direction: 'alternate',
+        // iterations: Infinity,
+        easing: 'ease-out',
+    } 
+)
+}
+
+function animatedSlideOut(slide, direction) {
+    slide.style.display = "block";
+    var thisAnimation = slide.animate( 
+    [
+        { transform: 'translateX(0px)'},
+        { transform: `translateX(${ direction == 'right' ? '-500px':'500px' } )` },
+    ], 
+    {
+        duration: 600,
+        // direction: 'alternate',
+        // iterations: Infinity,
+        easing: 'ease-out',
+    } 
+    )
+    thisAnimation.finished.then( function() { slide.style.display = 'none' } )
+}
+
+
